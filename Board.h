657e0f1f931ee71d1480469da866f3ff9ed541c4
JoMiras <jonamiraso@gmail.com>
2024-09-04 01:45:06 -0700

    I got tired so I only got to Board.h barely

---
+++ Board.h
@@ -0,0 +1,11 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <vector>
+
+const int boardWidth = 10;
+const int boardHeight = 20;
+const char emptyCell - '.';
+const char filledCell = '#';
+
+// Now I'm tired I wanna continue later lol
