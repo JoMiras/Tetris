cdc722d226cc4edb56a23f6d4985ea2ae5de9cee
JoMiras <jonamiraso@gmail.com>
2024-09-19 11:37:51 -0700

    Some more stuff

--- Game.h
+++ Game.h
@@ -1,4 +1,18 @@
 #ifndef GAME_H
 #define GAME_H 
 
-// Will complete the rest later
+#include "Board.h"
+#include "Pieces.h"
+#include <utility>
+
+class Game {
+    public: 
+    Game();
+    ~Game();
+
+    void run();
+
+    private: 
+    // I finish later
+    
+}
